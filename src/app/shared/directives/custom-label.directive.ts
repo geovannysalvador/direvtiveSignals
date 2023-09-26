ba141cb9c81a2223ff798db4efa57c2d544817eb
GeovannyTS <yowtecum@gmail.com>
2023-09-26 15:57:54 -0600

    send a value to an html tag(span)

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,13 +1,24 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[customLabel]'
 })
-export class CustomLabelDirective  {
+export class CustomLabelDirective implements OnInit  {
 
-  constructor() {
-    console.log('constructor de directiva');
+  private htmlElement?:ElementRef<HTMLElement>
 
+  constructor( private el:ElementRef<HTMLElement> ) {
+    // console.log('constructor de directiva');
+    console.log(el);
+    this.htmlElement = el;
+
+    // manda el valor de hola muno al span del html
+    // this.htmlElement.nativeElement.innerHTML = 'Hola mundo'
    }
+
+  ngOnInit(): void {
+    console.log('Directiva -NGOnInit');
+
+  }
 
 }
