3921e37fb56132753317fddbbae883107eae547f
GeovannyTS <yowtecum@gmail.com>
2023-09-27 11:25:40 -0600

    implement signal using counter page

--- src/app/signals/pages/counter-page/counter-page.component.ts
+++ src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   templateUrl: './counter-page.component.html',
@@ -6,4 +6,17 @@ import { Component } from '@angular/core';
 })
 export class CounterPageComponent {
 
+  public counter = signal(10);
+  // Signal de solo lectura no se puede manipular
+  public counterSquared = computed( ()=> this.counter() * this.counter() );
+
+  // Para el boton +1 y -1
+  increaseBy(value:number){
+    
+    // forma uno de hcerlo
+    // this.counter.set(this.counter() + value )
+
+    // Forma dos
+    this.counter.update( current => current + value);
+  }
 }
